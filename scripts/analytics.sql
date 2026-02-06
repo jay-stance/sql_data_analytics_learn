e467534617517b4066daca71e788756ee42f333f
Jau Stance <jaystance25@gmail.com>
2026-02-06 04:47:58 +0100

    dimensions and measures - exploring database and tables

---
+++ scripts/analytics.sql
@@ -0,0 +1,33 @@
+/*
+  DIMENTIONS AND MEASURES
+
+  is it Numeric?
+  NO: it is a dimension
+  YES: 
+    Does it make sense to aggregate it?
+    YES: it is a measure
+    NO: it is a dimension
+
+  - why do we need dimensions and measures?
+  we need dimensions for grouping the data (S axios in a chart), and we need measure for answering how much, quantifying data, to what measure is this dimension (Y axis
+  
+  
+  STEPS TAKEN IN DOING ANALYTICS FOR DATASETS
+  
+  - data exploratio: just to know what we have here, the number of tables, columns present etc)
+*/
+
+-- Create the database if it does not exist
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'DataWarehouseAnalytics')
+BEGIN
+    CREATE DATABASE DataWarehouseAnalytics;
+END
+
+USE DataWarehouseAnalytics;
+
+-- Explore all objects in the database
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- Explore all columns in the database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_NAME = 'dim_customers'
